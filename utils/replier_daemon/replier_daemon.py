237add62b0b999a0fc747fa89527ee6ad3fb78a8
exelay <exelay.rapik@gmail.com>
2021-06-15 14:27:14 +0300

    Fix getting notifications error

--- utils/replier_daemon/replier_daemon.py
+++ utils/replier_daemon/replier_daemon.py
@@ -13,28 +13,32 @@ async def main():
     repliers = [(account.id, LBClient(account.hmac_key, account.hmac_secret)) for account in accounts]
     for account_id, client in repliers:
         logger.debug(f"Подключен аккаунт {account_id}")
-        notifications = client.get_notifications()
-        new_offers = [
-            notification for notification in notifications
-            if notification['msg'].startswith('Вы получили новое предложение от') and not notification['read']
-        ]
-        replier = await get_replier(account_id)
-        if replier:
-            logger.debug(f"Работает автоответчик {replier.name}")
-            payment_info = replier.payment_info
-            reply_msg = (
-                "|\n"
-                "Привет!\n"
-                "Одним платежом на\n\n"
-                f"{payment_info}\n\n"
-            )
-            for offer in new_offers:
-                logger.debug(f"{offer}")
-                contact_id = offer['contact_id']
-                offer_id = offer['id']
-                client.post_message_to_contact(contact_id, reply_msg)
-                client.send_request(f'/api/notifications/mark_as_read/{offer_id}/', 'post')
-                logger.debug(f"Ответил на заявку {offer_id} реквизитами {payment_info}")
+        try:
+            notifications = client.get_notifications()
+        except KeyError:
+            logger.warning(f"Не удалось получить уведомления с аккаунта {account_id}")
+        else:
+            new_offers = [
+                notification for notification in notifications
+                if notification['msg'].startswith('Вы получили новое предложение от') and not notification['read']
+            ]
+            replier = await get_replier(account_id)
+            if replier:
+                logger.debug(f"Работает автоответчик {replier.name}")
+                payment_info = replier.payment_info
+                reply_msg = (
+                    "|\n"
+                    "Привет!\n"
+                    "Одним платежом на\n\n"
+                    f"{payment_info}\n\n"
+                )
+                for offer in new_offers:
+                    logger.debug(f"{offer}")
+                    contact_id = offer['contact_id']
+                    offer_id = offer['id']
+                    client.post_message_to_contact(contact_id, reply_msg)
+                    client.send_request(f'/api/notifications/mark_as_read/{offer_id}/', 'post')
+                    logger.debug(f"Ответил на заявку {offer_id} реквизитами {payment_info}")
 
 
 def run_replier():
